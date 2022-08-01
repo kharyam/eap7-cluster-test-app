72e38bf735bf6f7be249ebf227cba48d1618e11c
Khary Mendez <kmendez@redhat.com>
2022-08-01 16:45:13 -0400

    Update serial id

--- src/main/java/org/jboss/as/quickstarts/kitchensinkjsp/model/Counter.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensinkjsp/model/Counter.java
@@ -1,12 +1,18 @@
 package org.jboss.as.quickstarts.kitchensinkjsp.model;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
-
+//import org.jboss.ejb3.annotation.Clustered;
 @SessionScoped
-public class Counter {
+
+public class Counter implements Serializable{
     private int count;
+
+    private static final long serialVersionUID = -1;
+    
 
     @Produces
     @Named
